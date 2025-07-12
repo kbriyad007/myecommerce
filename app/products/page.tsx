e410c577233e4ab03a98a4a676bc0f60d26efb40
kbriyad007 <kb.riyad007@gmail.com>
2025-07-12 23:17:30 +0600

    page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -124,7 +124,7 @@ export default function Page() {
       />
       <HeroSection />
 
-     <div className="px-2 py-8 lg:px-4 lg:py-16 max-w-7xl mx-auto">
+     <div className="px-2 py-2 lg:px-4 lg:py-16 max-w-7xl mx-auto">
         <h1 className="text-4xl font-bold text-center text-gray-800 mb-12">
           ✨ Featured Products
         </h1>
